02c46b0d65d0a061fb0d2405c985fa02f3417871
Familia <andmej@gmail.com>
2008-11-29 23:11:33 -0500

    Vista bonita de index de judge

--- app/models/judge.rb
+++ app/models/judge.rb
@@ -1,2 +1,17 @@
 class Judge < ActiveRecord::Base
+  validates_presence_of :name, :url
+  validate :url_is_valid
+
+  protected
+  def url_is_valid
+    begin
+      uri = URI.parse(url)
+      if uri.class != URI::HTTP
+        errors.add(:url, 'Only HTTP protocol addresses can be used')
+      end
+    rescue URI::InvalidURIError
+      errors.add(:url, 'The format of the url is not valid.')
+    end
+  end
+
 end
