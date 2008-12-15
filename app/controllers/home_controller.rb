ebf45ef7ec2a0fd4dc6714a2fdec469fac1c8959
Andrés Mejía <andmej@gmail.com>
2008-12-15 16:47:38 -0500

    Adding some numbers on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@
 class HomeController < ApplicationController
   def index
     if logged_in?
-      @missing_problems = current_user.missing_problems
-      @unsolved_problems = Problem.unsolved_problems
-      @solved_problems = current_user.solved_problems
+      @missing_problems = current_user.missing_problems.uniq
+      @unsolved_problems = Problem.unsolved_problems.uniq
+      @solved_problems = current_user.solved_problems.uniq
     end
   end
 
