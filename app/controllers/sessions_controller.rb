8295a12e2c158c51f63439dd2b96e24573fe76c8
Nicolás Hock Isaza <nhocki@gmail.com>
2010-03-15 08:51:45 -0500

    Testing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
         cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
       end
       redirect_back_or_default('/')
-      flash[:notice] = "Logged in successfully"
+      flash[:notice] = "Logged in successfully!"
     else
       flash[:error] = "Authentication failed"
       render :action => 'new'
